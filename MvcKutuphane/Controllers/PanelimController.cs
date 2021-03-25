eb8d34552f1356c0b6a8b987690d75c0eac47aac
Yiğit Cumhur Erkan <erkan.cumhur@hotmail.com>
2021-03-25 23:53:57 +0300

    Mailbox

--- MvcKutuphane/Controllers/PanelimController.cs
+++ MvcKutuphane/Controllers/PanelimController.cs
@@ -20,7 +20,8 @@ namespace MvcKutuphane.Controllers
         public ActionResult Index()
         {
             var uyemail = (string)Session["Mail"];
-            var degerler = db.TBLUYELER.FirstOrDefault(z => z.MAIL == uyemail);
+            // var degerler = db.TBLUYELER.FirstOrDefault(z => z.MAIL == uyemail);
+            var degerler = db.TBLDUYURULAR.ToList();
             var d1 = db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.AD).FirstOrDefault();
             ViewBag.d1 = d1;
             var d2 = db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.SOYAD).FirstOrDefault();
@@ -35,7 +36,13 @@ namespace MvcKutuphane.Controllers
             ViewBag.d6 = d6;
             var d7 = db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.MAIL).FirstOrDefault();
             ViewBag.d7 = d7;
-
+            var uyeid = db.TBLUYELER.Where(x => x.MAIL == uyemail).Select(y => y.ID).FirstOrDefault();
+            var d8 = db.TBLHAREKET.Where(x => x.UYE == uyeid).Count();
+            ViewBag.d8 = d8;
+            var d9 = db.TBLMESAJLAR.Where(x => x.ALICI == uyemail).Count();
+            ViewBag.d9 = d9;
+            var d10 = db.TBLDUYURULAR.Count();
+            ViewBag.d10 = d10;
             return View(degerler);
         }
         [HttpPost]
@@ -72,6 +79,14 @@ namespace MvcKutuphane.Controllers
         public PartialViewResult Partial1()
         {
             return PartialView();
+        }
+        public PartialViewResult Partial2()
+        {
+            var kullanici = (string)Session["Mail"];
+            var id = db.TBLUYELER.Where(x => x.MAIL == kullanici).Select(y => y.ID).FirstOrDefault();
+            var uyegetir = db.TBLUYELER.Find(id);
+
+            return PartialView("Partial2", uyegetir);
         }
     }
 }
